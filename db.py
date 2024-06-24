8c6feaec3c0fcb51287270b0272f46e6f11da7c3
blue4683 <an4683@gmail.com>
2024-06-24 19:18:47 +0900

    initialize db setting

---
+++ db.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+import os
+import pymysql
+import pymysql.cursors
+
+load_dotenv()
+USERID = os.getenv("USERID")
+PASSWORD = os.getenv("PASSWORD")
+
+
+class Connection:
+    def __init__(self):
+        self.host = '127.0.0.1'
+        self.user = USERID
+        self.password = PASSWORD
+        self.db = 'nyanbit_db'
+        self.port = 3306
+        self.charset = 'utf8'
+        self.conn = pymysql.connect(
+            host=self.host, user=self.user, password=self.password,
+            db=self.db, port=self.port, charset=self.charset)
+        self.cur = self.conn.cursor(pymysql.cursors.DictCursor)
+        print('[알림] DB와 성공적으로 연결되었습니다.')
+
+    def __del__(self):
+        self.conn.close()
+        print('[알림] DB와의 연결을 끊었습니다.')
+
+    def get_connection(self):
+        self.conn.ping()
+        return self.conn, self.cur
